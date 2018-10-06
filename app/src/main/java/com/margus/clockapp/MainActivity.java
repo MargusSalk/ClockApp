21d4f129cfdd3b8bd3cf6ea63e15e2d99db4be70
Margus Salk <margus.salk@gmail.com>
2018-10-06 13:30:20 +0300

    Add basic clock activity

--- app/src/main/java/com/margus/clockapp/MainActivity.java
+++ app/src/main/java/com/margus/clockapp/MainActivity.java
@@ -1,13 +1,23 @@
 package com.margus.clockapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.buttonDisplayClock) {
+            Intent intentToOpenClockActivity = new Intent(MainActivity.this, ClockActivity.class);
+            startActivity(intentToOpenClockActivity);
+        }
     }
 }
